de5ed0db1ae4908e3acfb940f41db622a6d0faaa
Zaritsky Sergey <slancelord@gmail.com>
2022-02-22 21:38:25 +0500

    Добавить решения и доделать проверку

--- area.go
+++ area.go
@@ -55,18 +55,58 @@ func input() (a, b, c, AB, AC, BC float64, err error) {
 
 	if a <= 0 || b <= 0 || c <= 0 || AB <= 0 || AC <= 0 || BC <= 0 || AB+AC+BC >= 360 {
 		err = fmt.Errorf("Value entered incorrectly")
-		return
 	}
+
+	if a+b <= c || a+c <= b || c+b <= a {
+		err = fmt.Errorf("No such triangle exists")
+	}
+
+	if !math.IsNaN(AB) && !math.IsNaN(AC) && !math.IsNaN(BC) {
+		if AB >= 180 || AC >= 180 || BC >= 180 || AB+AC+BC != 360 {
+			err = fmt.Errorf("No such triangle exists")
+		}
+	} else if AB >= 180 || AC >= 180 || BC >= 180 || AB+AC+BC > 360 {
+		err = fmt.Errorf("No such triangle exists")
+	}
+
 	return
 
 }
 
-func area(a, b, c, AB, AC, BC float64) (S float64) {
-	if a == a && b == b && c == c {
+func areaAngle(AB, a, b float64) float64 {
+	return (0.5 * a * b * math.Sin((math.Pi*AB)/180))
+}
+
+func areaTWOAngle(AB, AC, a float64) float64 {
+	return a * a * math.Sin((math.Pi*AB)/180) * math.Sin((math.Pi*AC)/180) * 0.5 / math.Sin((math.Pi*(180-(AB+AC)))/180)
+}
+
+func area(a, b, c, AB, AC, BC float64) float64 {
+	if !math.IsNaN(a) && !math.IsNaN(b) && !math.IsNaN(c) {
 		p := (a + b + c) / 2
-		S = math.Sqrt(p * (p - a) * (p - b) * (p - c))
+		return math.Sqrt(p * (p - a) * (p - b) * (p - c))
 	}
-	return
+
+	if !math.IsNaN(AB) && !math.IsNaN(a) && !math.IsNaN(b) {
+		return areaAngle(AB, a, b)
+	}
+	if !math.IsNaN(AC) && !math.IsNaN(a) && !math.IsNaN(c) {
+		return areaAngle(AC, a, c)
+	}
+	if !math.IsNaN(BC) && !math.IsNaN(b) && !math.IsNaN(c) {
+		return areaAngle(BC, b, c)
+	}
+
+	if !math.IsNaN(AB) && !math.IsNaN(AC) && !math.IsNaN(a) {
+		return areaTWOAngle(AB, AC, a)
+	}
+	if !math.IsNaN(AC) && !math.IsNaN(BC) && !math.IsNaN(c) {
+		return areaTWOAngle(AC, BC, c)
+	}
+	if !math.IsNaN(AB) && !math.IsNaN(BC) && !math.IsNaN(b) {
+		return areaTWOAngle(AB, BC, b)
+	}
+	return math.NaN()
 }
 
 func main() {
@@ -83,6 +123,9 @@ func main() {
 		}
 	}
 
-	fmt.Print(area(a, b, c, AB, AC, BC))
-
+	if !math.IsNaN(area(a, b, c, AB, AC, BC)) {
+		fmt.Print(area(a, b, c, AB, AC, BC))
+	} else {
+		fmt.Print("Unable to calculate area insufficient data")
+	}
 }
